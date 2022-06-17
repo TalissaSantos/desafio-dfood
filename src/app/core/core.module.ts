f7f63e3d27bf110bff661fe6cffbf8c7bafaa03b
TalissaSantos <talissasantos15@hotmail.com>
2022-06-17 09:07:16 -0300

    criado a estilzacao

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/components/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent]
+})
+export class CoreModule {}
