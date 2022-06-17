5c076075eb16b11a918fc73a0bb4b21ca06f2ea3
TalissaSantos <talissasantos15@hotmail.com>
2022-06-17 09:04:55 -0300

    pasta atualizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavbarComponent } from './core/header/components/navbar/navbar.component';
 
-const routes: Routes = [];
+import { HomeModule } from './pages/home/home.module';
+
+// quando o path for vazio joga pro home
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => HomeModule
+  },
+  {
+    path: 'home',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+
+]
+RouterModule.forRoot([
+  {path:'inicio', component: NavbarComponent},
+  {path:'receita',component: FooterComponent},
+])
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
