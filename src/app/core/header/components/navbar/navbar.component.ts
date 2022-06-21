2cfec5bf121c3d75c0ef3774f4e349a4a738e887
TalissaSantos <talissasantos15@hotmail.com>
2022-06-21 08:59:26 -0300

    alteracao na pasta

--- src/app/core/header/components/navbar/navbar.component.ts
+++ src/app/core/header/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  menus: string[] = ['Inicio', 'Receitas', 'Contato'];
+  menus: string[] = ['Home', 'Crie uma receita'];
 
   constructor() {}
 
