31b3a5f677acf4a774d76ec08d6182e9b1e1a77f
TalissaSantos <talissasantos15@hotmail.com>
2022-06-21 09:01:41 -0300

    diretorio para deixar os cards dinamico

---
+++ src/app/pages/home/card-recipe/card-recipe.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CardHome } from 'src/models/cards-interface';
+
+@Component({
+  selector: 'app-card-recipe',
+  templateUrl: './card-recipe.component.html',
+  styleUrls: ['./card-recipe.component.scss']
+})
+export class CardRecipeComponent implements OnInit {
+
+
+
+
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
