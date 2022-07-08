172c09b248f191eeb46ae539959bd7af0a04204a
TalissaSantos <talissasantos15@hotmail.com>
2022-07-08 10:42:34 -0300

    updat

--- src/app/pages/form-recipes/form-recipes.component.ts
+++ src/app/pages/form-recipes/form-recipes.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/services/categories.service';
-import { FormValidations } from 'src/app/services/form-validations';
 import { Categories } from 'src/models/categories-interface';
+import { Recipe } from 'src/models/recipes-interface';
 
 @Component({
   selector: 'app-form-recipes',
@@ -12,19 +12,30 @@ import { Categories } from 'src/models/categories-interface';
 export class FormRecipesComponent implements OnInit {
   form!: FormGroup;
   categories!: Categories[];
+  submitted = false;
+  ingredients = new FormArray([new FormControl('',Validators.required)])
 
+
+  onAdd(){
+    this.ingredients.push(new FormControl('',Validators.required));
+  }
+
+  onRemove(i:number){
+this.ingredients.removeAt(i);
+  }
   constructor(private formBuilder: FormBuilder, private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      category: [null, [FormValidations.equalsTo('category')]],
+      category: [null, Validators.required],
+
       image: [null, Validators.required],
       recipe: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(35)]],
       createdBy: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
       serves: [null, [Validators.required, Validators.minLength(5)]],
       preparationTime: [null, [Validators.required, Validators.minLength(5)]],
       ingredients: this.buildIngredients(),
-      methodOfPreparation: this.buildMethodOfPreparations()
+      methodOfPreparation: [null, Validators.required]
     });
 
     this.categoriesService.getCategory().subscribe({
@@ -32,24 +43,31 @@ export class FormRecipesComponent implements OnInit {
     });
   }
 
-
   verificaValidTouched(campo: string) {
-    return (
-      !this.form.get(campo)?.valid &&
-      !!this.form.get(campo)?.touched
-    );
+    return !this.form.get(campo)?.valid && (this.form.get(campo)?.touched || this.form.get(campo)?.dirty);
   }
 
   //formArray ingredientes
   buildIngredients() {}
-  //formArray modo de preparo
-  buildMethodOfPreparations() {}
 
   onSubmit() {
+    this.submitted = true;
     console.log(this.form.value);
+    if (this.form.valid) {
+      console.log('ok');
+
+    } else {
+      Object.keys(this.form.controls).forEach(campo => {
+        console.log(campo);
+        const controle = this.form.get(campo);
+        controle?.markAsDirty();
+      });
+
+    }
   }
+
   onCancel() {
+    this.submitted = false;
     this.form.reset();
-    confirm('Deseja cancelar?');
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,25 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs';
 import { Categories } from 'src/models/categories-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
-
   categories!: Categories[];
 
 
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) { }
+  getCategory() {
+    return this.http.get<Categories[]>('assets/dados/mock.json')
 
-getCategory() {
-  return this.http.get<Categories[]>('assets/dados/mock.json');
-}
-
-
-
-
+  }
 
 }
-
